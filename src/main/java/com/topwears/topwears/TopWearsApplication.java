80e8d61726a1db18fa64b71918f195c57746b617
CourageOghogho <oghogho.courage@gmail.com>
2022-11-28 01:52:16 +0100

    init

---
+++ src/main/java/com/topwears/topwears/TopWearsApplication.java
@@ -0,0 +1,13 @@
+package com.topwears.topwears;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopWearsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopWearsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/topwears/topwears/TopWearsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topwears.topwears;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopWearsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
